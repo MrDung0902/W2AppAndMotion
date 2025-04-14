8070e5d1a2e5c1408ab3026cf1b2b24123b7ea0d
Mr Dung <hualduc@gmail.com>
2025-04-14 16:35:31 +0700

    Add project files.

---
+++ W2AppAndMotion/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        if (args.Length > 0)
+        {
+            Console.WriteLine($"Xin chào, {args[0]}!");
+        }
+        else
+        {
+            Console.WriteLine("Không có tên nào được truyền trong args.");
+        }
+
+        Console.ReadLine(); // Chờ nhấn phím
+    }
+}
