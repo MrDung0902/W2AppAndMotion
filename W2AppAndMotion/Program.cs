5ec46504a51f2e9c5cf86e01619528b4a421a723
Mr Dung <hualduc@gmail.com>
2025-04-14 16:45:53 +0700

    RE nhanh

--- W2AppAndMotion/Program.cs
+++ W2AppAndMotion/Program.cs
@@ -8,6 +8,7 @@ class Program
         {
             Console.WriteLine($"Xin chào, {args[0]}!");
             Console.WriteLine("HELLO");
+            Console.WriteLine("123");
         }
         else
         {
